c091e23bec8ea67bda83198423dca115fa55659b
matt-lourens <matt.lourens.123@gmail.com>
2022-08-17 08:43:41 +0100

    Fixed another ordering issue

--- quantum_estimators/utils.py
+++ quantum_estimators/utils.py
@@ -1,3 +1,4 @@
+# %%
 import numpy as np
 import networkx as nx
 import matplotlib.pyplot as plt
@@ -15,17 +16,16 @@ def plot_graph(
     """
     n_qbits = len(graph.Q)
     # Change order around a circle, this way you start at x=0 then move left around
-    theta_0 = 2 / 8 # specify vector(0,1) as start
+    theta_0 = 2 / 8  # specify vector(0,1) as start
     theta_step = 1 / n_qbits
     pos = {
-        ind
-        + 1: np.array(
+        label: np.array(
             [
                 np.cos(2 * np.pi * (theta_0 + ind * theta_step)),
                 np.sin(2 * np.pi * (theta_0 + ind * theta_step)),
             ]
         )
-        for ind in range(n_qbits)
+        for label, ind in zip(graph.Q, range(n_qbits))
     }
     nx_graph = nx.DiGraph()
     nx_graph.add_nodes_from(graph.Q)
@@ -52,7 +52,7 @@ def plot_graph(
         width=1.5,
         **kwargs,
     )
-# %%
+
 def plot_qcnn_graphs(graphs, conv_color="#0096ff", pool_color="#ff7e79", **kwargs):
     # Get the first graph to derive some relevant information from the structure
     Qc_0 = list(graphs.values())[0][0][0]
