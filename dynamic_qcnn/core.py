94d0e00914b053614ac027f4992ca6186eab9f98
matt-lourens <matt.lourens.123@gmail.com>
2022-10-27 00:42:05 +0200

    cleanup

--- dynamic_qcnn/core.py
+++ dynamic_qcnn/core.py
@@ -5,7 +5,6 @@ from copy import copy, deepcopy
 from collections import deque
 import numpy as np
 import itertools as it
-from qutip.qip.circuit import QubitCircuit, Gate
 import cirq
 
 
@@ -474,252 +473,35 @@ class LinkedDiGraph:
     def __call__(self, prev_graph):
         self.prev_graph = prev_graph
 
-
-# print("debug")
-# # Pooling circuit
-# def V_1(bits, symbols=None):  # 1
-#     circuit = cirq.Circuit()
-#     q0, q1 = cirq.LineQubit(bits[0]), cirq.LineQubit(bits[1])
-#     circuit += cirq.rx(symbols[0]).on(q1).controlled_by(q0)
-
-#     return circuit
-
-
-# def V_2(bits, symbols=None):  # 0
-#     circuit = cirq.Circuit()
-#     q0, q1 = cirq.LineQubit(bits[0]), cirq.LineQubit(bits[1])
-#     circuit += cirq.CNOT(q0, q1)
-#     return circuit
-
-
-# # Convolution circuit
-# def U_1(bits, symbols=None):  # 1
-#     circuit = cirq.Circuit()
-#     q0, q1 = cirq.LineQubit(bits[0]), cirq.LineQubit(bits[1])
-#     circuit += cirq.rz(symbols[0]).on(q1).controlled_by(q0)
-#     return circuit
-
-
-# def U_2(bits, symbols=None):  # 1
-#     circuit = cirq.Circuit()
-#     q0, q1 = cirq.LineQubit(bits[0]), cirq.LineQubit(bits[1])
-#     circuit += cirq.ry(symbols[0]).on(q1).controlled_by(q0)
-#     return circuit
-
-
-# function_mappings = {
-#     "convolution": [(U_1, 1), (U_2, 1)],
-#     "pooling": [(V_1, 1), (V_2, 0)],
-# }
-# qcnn = Qcnn(16, function_mappings=function_mappings)
-
-# a = qcnn + (Qconv() + Qpool()) * 3
-
-# import random
-# import operator
-# from functools import reduce
-# from dynamic_qcnn import convert_graph_to_circuit_cirq
-
-# N = 8
-
-# p = [
-#     Qpool(s_p, filter=i_filter)
-#     for i_filter in (
-#         "right",
-#         "left",
-#         "even",
-#         "odd",
-#         "inside",
-#         "outside",
-#         lambda arr: [arr[-1]],
-#         lambda arr: [arr[0]],
-#     )
-#     for s_p in range(N)
-# ]
-# c = [Qconv(s_c, s_t,so) for s_c in range(N) for s_t in range(1, N, 1) for so in range(N)]
-# m2_1 = [random.choice(c) + random.choice(p) for i in range(int(np.log2(N)))]
-# m2_2 = [random.choice(c) + random.choice(c) for i in range(int(np.log2(N)))]
-# m3_1 = reduce(operator.add, m2_1)
-# Qfree(N) + m3_1
-# circuit, symbols = convert_graph_to_circuit_cirq(Qfree(N) + m3_1)
-# construct reverse binary tree
-def binary_tree_r(
-    n_q=8,
-    s_c=1,
-    s_p=0,
-    pool_filter="right",
-    convolution_mapping=None,
-    pooling_mapping=None,
-):
-    tail_graph = n_q
-    for layer in range(1, int(np.log2(n_q)) + 1, 1):
-        # Convolution
-        if not (convolution_mapping is None):
-            convolution_l = convolution_mapping.get(layer, convolution_mapping[1])
-        else:
-            convolution_l = None
-        tail_graph = Qconv(tail_graph, stride=s_c, convolution_mapping=convolution_l)
-        if tail_graph.prev_graph is None:
-            # Set first graph, i.e. first layer first convolution
-            head_graph = tail_graph
-        # Pooling
-        if not (pooling_mapping is None):
-            pooling_l = pooling_mapping.get(layer, pooling_mapping[1])
-        else:
-            pooling_l = None
-        tail_graph = Qpool(
-            tail_graph,
-            stride=s_p,
-            pool_filter=pool_filter,
-            pooling_mapping=pooling_l,
-        )
-    return head_graph, tail_graph
-
-
-# class QConv(QMotif):
-#     def __init__(self, prev_graph, stride=1, convolution_mapping=None):
-
-#         # TODO repr functions for both conv and pool
-#         # TODO graphing functions for both
-#         if isinstance(prev_graph, Sequence):
-#             # Qc_l is given as a sequence: list, tuple or range object.
-#             if isinstance(prev_graph[-1], Q_Primitive):
-#                 prev_graph[-1].set_next(self)
-#                 Qc_l = prev_graph[-1].Q_avail
-#             elif type(prev_graph[-1]) == int:
-#                 # Assume number of qubits were specified as prev_graph for first layer
-#                 Qc_l = list(prev_graph)
+# def binary_tree_r(
+#     n_q=8,
+#     s_c=1,
+#     s_p=0,
+#     pool_filter="right",
+#     convolution_mapping=None,
+#     pooling_mapping=None,
+# ):
+#     tail_graph = n_q
+#     for layer in range(1, int(np.log2(n_q)) + 1, 1):
+#         # Convolution
+#         if not (convolution_mapping is None):
+#             convolution_l = convolution_mapping.get(layer, convolution_mapping[1])
 #         else:
-#             if isinstance(prev_graph, Q_Primitive):
-#                 prev_graph.set_next(self)
-#                 Qc_l = prev_graph.Q_avail
-#             elif type(prev_graph) == int:
-#                 # Assume number of qubits were specified as prev_graph for first layer
-#                 Qc_l = [i + 1 for i in range(prev_graph)]
-#         # elif isinstance(prev_graph, Sequence):
-#         #     # Qc_l is given as a sequence: list, tuple or range object.
-
-#         #     Qc_l = list(prev_graph)
-#         # else:
-#         #     TypeError(
-#         #         f"prev_graph needs to be int, sequence or Q_Primitive, recieved {type(prev_graph)}"
-#         #     )
-
-#         self.type = "convolution"
-#         self.stride = stride
-#         self.Q_avail = Qc_l
-#         # Determine convolution operation
-#         nq_avaiable = len(Qc_l)
-#         if nq_avaiable == stride:
-#             raise ValueError(
-#                 f"Stride and number of avaiable qubits can't be the same, recieved:\nstride: {stride}\navaiable qubits:{nq_avaiable}"
-#             )
-#         mod_nq = lambda x: x % nq_avaiable
-#         Ec_l = [(Qc_l[i], Qc_l[mod_nq(i + self.stride)]) for i in range(nq_avaiable)]
-#         if len(Ec_l) == 2 and Ec_l[0][0:] == Ec_l[1][1::-1]:
-#             Ec_l = [Ec_l[0]]
-#         # Specify sequence of gates:
-#         if convolution_mapping is None:
-#             # default convolution layer is defined as U with 10 paramaters.
-#             convolution_mapping = (U, 1)
-#         # Initialize graph
-#         super().__init__(Qc_l, Ec_l, prev_graph, function_mapping=convolution_mapping)
-
-# class QPool(Q_Primitive):
-#     def __init__(self, prev_graph, stride=0, pool_filter="right", pooling_mapping=None):
-#         if isinstance(prev_graph, Q_Primitive):
-#             Qp_l = prev_graph.Q_avail
-#         elif type(prev_graph) == int:
-#             # Assume number of qubits were specified as prev_graph for first layer
-#             Qp_l = [i + 1 for i in range(prev_graph)]
-#         elif isinstance(prev_graph, Sequence):
-#             # Qc_l is given as a sequence: list, tuple or range object.
-#             Qp_l = list(prev_graph)
+#             convolution_l = None
+#         tail_graph = Qconv(tail_graph, stride=s_c, convolution_mapping=convolution_l)
+#         if tail_graph.prev_graph is None:
+#             # Set first graph, i.e. first layer first convolution
+#             head_graph = tail_graph
+#         # Pooling
+#         if not (pooling_mapping is None):
+#             pooling_l = pooling_mapping.get(layer, pooling_mapping[1])
 #         else:
-#             TypeError(
-#                 f"prev_graph needs to be int, sequence or Q_Primitive, recieved {type(prev_graph)}"
-#             )
-#         if len(Qp_l) > 1:
-#             if isinstance(prev_graph, Q_Primitive):
-#                 prev_graph.set_next(self)
-#             self.type = "pooling"
-#             self.stride = stride
-#             self.pool_filter_fn = self.get_pool_filter_fn(pool_filter)
-#             measured_q = self.pool_filter_fn(Qp_l)
-#             remaining_q = [q for q in Qp_l if not (q in measured_q)]
-#             Ep_l = [
-#                 (measured_q[i], remaining_q[(i + self.stride) % len(remaining_q)])
-#                 for i in range(len(measured_q))
-#             ]
-#             self.Q_avail = remaining_q
-#             # Specify sequence of gates:
-#             if pooling_mapping is None:
-#                 pooling_mapping = (V, 0)
-#             # Initialize graph
-#             super().__init__(Qp_l, Ep_l, prev_graph, function_mapping=pooling_mapping)
-#         else:
-#             raise ValueError(
-#                 "Pooling operation not added, Cannot perform pooling on 1 qubit"
-#             )
+#             pooling_l = None
+#         tail_graph = Qpool(
+#             tail_graph,
+#             stride=s_p,
+#             pool_filter=pool_filter,
+#             pooling_mapping=pooling_l,
+#         )
+#     return head_graph, tail_graph
 
-#     def get_pool_filter_fn(self, pool_filter):
-#         if type(pool_filter) is str:
-#             # Mapping words to the filter type
-#             if pool_filter == "left":
-#                 # 0 1 2 3 4 5 6 7
-#                 # x x x x
-#                 pool_filter_fn = lambda arr: arr[0 : len(arr) // 2 : 1]
-#             elif pool_filter == "right":
-#                 # 0 1 2 3 4 5 6 7
-#                 #         x x x x
-#                 pool_filter_fn = lambda arr: arr[len(arr) : len(arr) // 2 - 1 : -1]
-#             elif pool_filter == "even":
-#                 # 0 1 2 3 4 5 6 7
-#                 # x   x   x   x
-#                 pool_filter_fn = lambda arr: arr[0::2]
-#             elif pool_filter == "odd":
-#                 # 0 1 2 3 4 5 6 7
-#                 #   x   x   x   x
-#                 pool_filter_fn = lambda arr: arr[1::2]
-#             elif pool_filter == "inside":
-#                 # 0 1 2 3 4 5 6 7
-#                 #     x x x x
-#                 pool_filter_fn = (
-#                     lambda arr: arr[
-#                         len(arr) // 2
-#                         - len(arr) // 4 : len(arr) // 2
-#                         + len(arr) // 4 : 1
-#                     ]
-#                     if len(arr) > 2
-#                     else [arr[1]]
-#                 )  # inside
-#             elif pool_filter == "outside":
-#                 # 0 1 2 3 4 5 6 7
-#                 # x x         x x
-#                 pool_filter_fn = (
-#                     lambda arr: [
-#                         item
-#                         for item in arr
-#                         if not (
-#                             item
-#                             in arr[
-#                                 len(arr) // 2
-#                                 - len(arr) // 4 : len(arr) // 2
-#                                 + len(arr) // 4 : 1
-#                             ]
-#                         )
-#                     ]
-#                     if len(arr) > 2
-#                     else [arr[0]]
-#                 )  # outside
-#             else:
-#                 # Assume filter is in form contains a string specifying which indices to remove
-#                 # For example "01001" removes idx 1 and 4 or qubit 2 and 5
-#                 # The important thing here is for pool filter to be the same length as the current number of qubits
-#                 # TODO add functionality to either pad or infer a filter from a string such as "101"
-#                 pool_filter_fn = lambda arr: [
-#                     item
-#                     for item, indicator in zip(arr, pool_filter)
-#                     if indicator == "1"
-#                 ]
-#             return pool_filter_fn
