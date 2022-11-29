02f55ba88a49d7284c0e19e925b1fb245b7a36fe
matt-lourens <matt.lourens.123@gmail.com>
2022-11-29 08:04:21 +0200

    added sympy functionality to pennylane tutorial

--- dynamic_qcnn/pennylane/pennylane_helper.py
+++ dynamic_qcnn/pennylane/pennylane_helper.py
@@ -1,6 +1,8 @@
-from dynamic_qcnn.core import Primitive_Types
 import warnings
+from sympy import symbols
 import pennylane as qml
+from dynamic_qcnn.core import Primitive_Types
+
 
 
 def V(bits, symbols=None):
@@ -11,7 +13,7 @@ def U(bits, symbols=None):
     qml.CRZ(symbols[0], wires=[bits[0], bits[1]])
 
 
-def convert_graph_to_circuit_pennylane(qcnn, symbols):
+def get_param_info_pennylane(qcnn):
     total_coef_count = 0
     coef_indices = {}
     ind = 0
@@ -34,10 +36,31 @@ def convert_graph_to_circuit_pennylane(qcnn, symbols):
                 total_coef_count, total_coef_count + block_param_count
             )
             total_coef_count = total_coef_count + block_param_count
-            for bits in layer.E:
-                block(bits=bits, symbols=symbols[coef_indices[ind]])
-            ind = ind + 1
         else:
-            for bits in layer.E:
-                block(bits=bits)
-    return coef_indices
+            coef_indices[ind] = None
+        ind = ind + 1
+        symbols_coef = symbols(f'x:{total_coef_count}')
+    return symbols_coef, total_coef_count, coef_indices
+
+
+def execute_circuit_pennylane(qcnn, symbols, coef_indices=None):
+    ind = 0
+    if coef_indices == None:
+        coef_indices = get_symbol_indices_pennylane(qcnn=qcnn)
+    for layer in qcnn:
+        if layer.is_default_mapping and layer.function_mapping == None:
+            if layer.type in [
+                Primitive_Types.CONVOLUTION.value,
+                Primitive_Types.DENSE.value,
+            ]:
+                layer.set_mapping((U, 1))
+            elif layer.type in [Primitive_Types.POOLING.value]:
+                layer.set_mapping((V, 0))
+            else:
+                warnings.warn(
+                    f"No default function mapping for primitive type: {layer.type}, please provide a mapping manually"
+                )
+        block, block_param_count = layer.function_mapping
+        for bits in layer.E:
+            block(bits=bits, symbols=symbols[coef_indices[ind]])
+        ind = ind + 1
