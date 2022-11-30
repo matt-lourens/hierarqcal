7e12b4f9f677794e6d222eba17d105c7dbc53540
matt-lourens <matt.lourens.123@gmail.com>
2022-11-30 14:04:56 +0200

    Removed symbol usage for pennylane

--- dynamic_qcnn/pennylane/pennylane_helper.py
+++ dynamic_qcnn/pennylane/pennylane_helper.py
@@ -1,8 +1,7 @@
 import warnings
-from sympy import symbols
+from sympy import symbols, lambdify
 import pennylane as qml
 from dynamic_qcnn.core import Primitive_Types
-
 
 
 def V(bits, symbols=None):
@@ -39,14 +38,13 @@ def get_param_info_pennylane(qcnn):
         else:
             coef_indices[ind] = None
         ind = ind + 1
-        symbols_coef = symbols(f'x:{total_coef_count}')
-    return symbols_coef, total_coef_count, coef_indices
+    return total_coef_count, coef_indices
 
 
-def execute_circuit_pennylane(qcnn, symbols, coef_indices=None):
+def execute_circuit_pennylane(qcnn, params, coef_indices=None):
     ind = 0
     if coef_indices == None:
-        coef_indices = get_symbol_indices_pennylane(qcnn=qcnn)
+        total_coef_count, coef_indices = get_param_info_pennylane(qcnn=qcnn)
     for layer in qcnn:
         if layer.is_default_mapping and layer.function_mapping == None:
             if layer.type in [
@@ -61,6 +59,11 @@ def execute_circuit_pennylane(qcnn, symbols, coef_indices=None):
                     f"No default function mapping for primitive type: {layer.type}, please provide a mapping manually"
                 )
         block, block_param_count = layer.function_mapping
-        for bits in layer.E:
-            block(bits=bits, symbols=symbols[coef_indices[ind]])
+        if coef_indices[ind] is None:
+            # If layer has no associated params
+            for bits in layer.E:
+                block(bits=bits, symbols=None)
+        else:
+            for bits in layer.E:
+                block(bits=bits, symbols=params[coef_indices[ind]])
         ind = ind + 1
