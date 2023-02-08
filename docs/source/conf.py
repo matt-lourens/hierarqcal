00eb3fec716c0db1eabc8402d75057abce6481f1
matt-lourens <matt.lourens.123@gmail.com>
2023-02-08 13:41:20 +0200

    favicon + spelling mistake

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -36,7 +36,7 @@ exclude_patterns = []
 html_theme = "sphinx_rtd_theme"
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 html_static_path = ["_static"]
-html_favicon = ".source/img/dalle_img.png"
+html_favicon = "./img/dalle_img.png"
 
 
 
