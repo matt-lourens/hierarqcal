1ff5cab005cbfb6f2438e44865492f31b9788f82
Aaron Robertson <aaronrobertson.dev@gmail.com>
2023-05-29 08:31:23 -0400

    default angle

--- hierarqcal/qiskit/qiskit_circuits.py
+++ hierarqcal/qiskit/qiskit_circuits.py
@@ -27,11 +27,45 @@ def U3(bits, symbols=None, circuit=None):
     circuit.crx(symbols[1], q2, q1)
     return circuit
 
+def H2(bits, symbols=None, circuit=None):
+    """
+    Example circuit for h_top using the Hadamard gate (H gate).
+
+    Args:
+        bits (list(string or int)): List of qubit indices/labels, if int then the qubits will be named :code:`f"q{bits[0]}" and f"q{bits[1]}"`
+        symbols (tuple(Parameter)): Tuple of symbol values (rotation angles) as a Qiskit Parameter object, can be symbolic or numeric.
+        circuit (qiskit.QuantumCircuit): QuantumCircuit object to add operations to, if None then a new QuantumCircuit object will be created.
+
+    Returns:
+        circuit (qiskit.QuantumCircuit): QuantumCircuit object
+    """
+    # Assume bits are strings and in the correct QASM format
+    q0, q1 = QuantumRegister(1, bits[0]), QuantumRegister(1, bits[1])
+    circuit.h(q0)
+    return circuit
+
+def CR2(bits, symbols=None, circuit=None):
+    """
+    Example circuit for u_cr using the controlled phase shift gate.
+
+    Args:
+        bits (list(string or int)): List of qubit indices/labels, if int then the qubits will be named :code:`f"q{bits[0]}" and f"q{bits[1]}"`
+        symbols (tuple(Parameter)): Tuple of symbol values (rotation angles) as a Qiskit Parameter object, can be symbolic or numeric.
+        circuit (qiskit.QuantumCircuit): QuantumCircuit object to add operations to, if None then a new QuantumCircuit object will be created.
+
+    Returns:
+        circuit (qiskit.QuantumCircuit): QuantumCircuit object
+    """
+    # Assume bits are strings and in the correct QASM format
+    q0, q1 = QuantumRegister(1, bits[0]), QuantumRegister(1, bits[1])
+    angle = symbols[0] if symbols else 0.0  # Use a different default symbol if no symbols are provided
+    circuit.cp(angle, q0, q1)
+    return circuit
 
 # Default mask circuit
 def V2(bits, symbols=None, circuit=None):
     """
-    Default mask circuit, a simple 2 qubit circuit with no parameters and a controlled controlled operation.
+    Default mask circuit, a simple 2 qubit circuit with no parameters and a controlled operation.
 
     Args:
         bits (list(string or int)): List of qubit indices/labels, if int then the qubit will be named :code:`f"q{bits[0]}" and f"q{bits[1]}"`
