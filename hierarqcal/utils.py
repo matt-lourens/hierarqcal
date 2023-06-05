69f2f86019de10a33aa0015dd70385e0db5a6161
Chaithanya Naik <chaithanyampcrl@gmail.com>
2023-06-05 13:36:56 -0500

    formatted with black

--- hierarqcal/utils.py
+++ hierarqcal/utils.py
@@ -30,7 +30,7 @@ def plot_motif(
     edge_width=1.5,
     figsize=(4, 4),
     font_size=15,
-    color_dict = {'start': "green", 'during': "orange", 'end': "red"},
+    color_dict={"start": "green", "during": "orange", "end": "red"},
     start_angle=0,  # given in 2 pi radians. 1/4 is 90 degrees, 1/2 is 180 degrees, 1 is 360 degrees
     **kwargs,
 ):
@@ -65,9 +65,7 @@ def plot_motif(
         ]
         node_colour = mask_color
     elif isinstance(primitive, Qunmask):
-        node_radi = [
-            node_large for q in primitive.Q_avail
-        ]        
+        node_radi = [node_large for q in primitive.Q_avail]
         node_colour = mask_color
         labels = primitive.Q_avail
         n_qbits = len(primitive.Q_avail)
@@ -80,7 +78,6 @@ def plot_motif(
     else:
         raise NotImplementedError(f"No plot specified for {primitive} primitive")
 
-    
     # Change order around a circle, this way you start at x=0 then move left around
     theta_0 = start_angle  # specify vector(1,0) as start
     theta_step = 1 / n_qbits
@@ -139,7 +136,7 @@ def plot_motif(
                 t_xy,
                 mutation_scale=30,
                 arrowstyle=patches.ArrowStyle(
-                    "-|>", head_length= 2*t_radius, head_width= t_radius
+                    "-|>", head_length=2 * t_radius, head_width=t_radius
                 ),
                 lw=edge_width,
                 fc="black",
@@ -150,74 +147,78 @@ def plot_motif(
             end_points = []
             for i in range(len(edge)):
                 ket_s = np.array(pos[edge[i]])  # source vector
-                ket_t = np.array(pos[edge[(i+1)%len(edge)]])  # target vector
+                ket_t = np.array(pos[edge[(i + 1) % len(edge)]])  # target vector
 
-                par_vec = ket_t-ket_s
-                
+                par_vec = ket_t - ket_s
+
                 ## Cyclic order dependency for clearer plotting
                 ## clockwise direction of nodes needs plotting to start from outer side
-                orth_vec = np.array([par_vec[1], -par_vec[0]])*(1/np.linalg.norm(par_vec))
-                
-                ## anticlockwise direction of nodes needs plotting to start from inner side 
+                orth_vec = np.array([par_vec[1], -par_vec[0]]) * (
+                    1 / np.linalg.norm(par_vec)
+                )
+
+                ## anticlockwise direction of nodes needs plotting to start from inner side
                 if primitive.arity < primitive.step:
                     orth_vec = -orth_vec
-                
+
                 t_radius = node_radi[labels.index(edge[i])]
-                
+
                 pad_ratio = 1.5
-                ket_s = ket_s + (t_radius*pad_ratio)*orth_vec
-                ket_t = ket_t + (t_radius*pad_ratio)*orth_vec
-                
+                ket_s = ket_s + (t_radius * pad_ratio) * orth_vec
+                ket_t = ket_t + (t_radius * pad_ratio) * orth_vec
+
                 path_trace = Path([ket_s, ket_t], [Path.MOVETO, Path.LINETO])
-                pathpatch_trace = PathPatch(path_trace, 
-                                            lw=edge_width,
-                                            color="black",
-                                            zorder=-1)
-                
+                pathpatch_trace = PathPatch(
+                    path_trace, lw=edge_width, color="black", zorder=-1
+                )
+
                 ax.add_patch(pathpatch_trace)
-                end_points.append((ket_s, ket_t, pos[edge[(i+1)%len(edge)]], t_radius*pad_ratio))
-            
+                end_points.append(
+                    (ket_s, ket_t, pos[edge[(i + 1) % len(edge)]], t_radius * pad_ratio)
+                )
+
             for i in range(len(end_points)):
-                
                 if i == 0:
-                    color = color_dict['start']
-                elif i == len(edge)-1:
-                    color = color_dict['end']
+                    color = color_dict["start"]
+                elif i == len(edge) - 1:
+                    color = color_dict["end"]
                 else:
-                    color = color_dict['during']
+                    color = color_dict["during"]
                 center = end_points[i][2]
                 start = end_points[i][1]
-                end = end_points[(i+1)%len(end_points)][0]
-                
+                end = end_points[(i + 1) % len(end_points)][0]
+
                 if primitive.arity < primitive.step:
                     start, end = end, start
-                
-                start_vec = start-center
-                end_vec = end-center
-                ref_vec = np.array([1,0]) 
-                
-                norm_prod = np.linalg.norm(start_vec)*np.linalg.norm(end_vec)
-                angle = np.arccos(np.dot(start_vec, end_vec)/norm_prod)
-                
-                if start_vec[1] < 0:
-                    norm_prod = np.linalg.norm(start_vec)*np.linalg.norm(ref_vec)
-                    angle_ref = -np.arccos(np.dot(start_vec, ref_vec)/norm_prod)
-                else:
-                    norm_prod = np.linalg.norm(start_vec)*np.linalg.norm(ref_vec)
-                    angle_ref = np.arccos(np.dot(start_vec, ref_vec)/norm_prod)
 
-                end_point_check = np.array([center[0] + t_radius*pad_ratio * np.cos(angle_ref+angle), 
-                                            center[1] + t_radius*pad_ratio * np.sin(angle_ref+angle)])
+                start_vec = start - center
+                end_vec = end - center
+                ref_vec = np.array([1, 0])
+
+                norm_prod = np.linalg.norm(start_vec) * np.linalg.norm(end_vec)
+                angle = np.arccos(np.dot(start_vec, end_vec) / norm_prod)
+
+                if start_vec[1] < 0:
+                    norm_prod = np.linalg.norm(start_vec) * np.linalg.norm(ref_vec)
+                    angle_ref = -np.arccos(np.dot(start_vec, ref_vec) / norm_prod)
+                else:
+                    norm_prod = np.linalg.norm(start_vec) * np.linalg.norm(ref_vec)
+                    angle_ref = np.arccos(np.dot(start_vec, ref_vec) / norm_prod)
+
+                end_point_check = np.array(
+                    [
+                        center[0] + t_radius * pad_ratio * np.cos(angle_ref + angle),
+                        center[1] + t_radius * pad_ratio * np.sin(angle_ref + angle),
+                    ]
+                )
                 if np.linalg.norm(end_point_check - end) > 1e-3:
-                    angle = 2*np.pi - angle
-                arc_angles = np.linspace(angle_ref, angle_ref+angle, 20)
-                arc_xs = center[0] + t_radius*pad_ratio * np.cos(arc_angles)
-                arc_ys = center[1] + t_radius*pad_ratio * np.sin(arc_angles)
+                    angle = 2 * np.pi - angle
+                arc_angles = np.linspace(angle_ref, angle_ref + angle, 20)
+                arc_xs = center[0] + t_radius * pad_ratio * np.cos(arc_angles)
+                arc_ys = center[1] + t_radius * pad_ratio * np.sin(arc_angles)
 
                 plt.plot(arc_xs, arc_ys, lw=edge_width, color=color)
-                
 
-                
     ax.set_aspect("equal", "box")
     # remove the x and y axis
     ax.axis("off")
