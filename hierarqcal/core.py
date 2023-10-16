65904a2b9343f4142f2bcf3724f2e0844ce444fc
matt-lourens <matt.lourens.123@gmail.com>
2023-10-16 14:09:08 +0200

    added reverse + pivot fix

--- hierarqcal/core.py
+++ hierarqcal/core.py
@@ -1203,7 +1203,7 @@ class Qpivot(Qsplit):
         if isinstance(offsets, int):
             offsets = [offsets] * 3
         if isinstance(boundaries, str):
-            offsets = [offsets] * 3
+            offsets = [boundaries] * 3
 
         super().__init__(
             global_pattern=global_pattern,
@@ -1286,7 +1286,9 @@ class Qpivot(Qsplit):
                 # Reorder E_p so that like pivots are grouped together, i.e. remaining avaliable qubits are assigned first to the first pivot point, then the second and so on.
                 E_tmp = []
                 for i in range(N):
-                    E_tmp += E_p[i::N]
+                    E_tmp += E_p[
+                        i :: N + 1
+                    ]  # TODO check if this change works as intended it used to be just N
                 E_p = E_tmp.copy()
 
                 # TODO what could merge_between be used for?
@@ -1593,7 +1595,6 @@ class Qhierarchy:
         Returns:
             Qhierarchy: A new Qhierarchy object with the two merged.
         """
-        # ensure immutability
         other_hierarchy = deepcopy(hierarchy)
         new_hierarchy = deepcopy(self)
         other_hierarchy.update_Q(
@@ -1617,6 +1618,19 @@ class Qhierarchy:
         for hierarchy in hierarchies:
             new_qcnn = new_qcnn.merge(hierarchy)
         return new_qcnn
+
+    def reverse(self):
+        """
+        Reverse the stack of motifs on the highest level, i.e. only the direct children nothing deeper.
+        """
+        old = self.head
+        current = deepcopy(self.tail)
+        while not (isinstance(old, Qinit)):
+            current = current + old
+            old = old.prev
+        current.head.set_next(None)
+
+        return current
 
     def update_Q(self, Q, start_idx=0):
         """
